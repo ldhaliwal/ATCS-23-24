bb9299f97385ed131a9c870d59d2d08395d8fb21
ldhaliwal <liliana.dhaliwal@menloschool.org>
2023-12-04 13:45:15 -0800

    finished fsm and objective 1 for the most part

---
+++ fsm.py
@@ -0,0 +1,26 @@
+"""
+@author: Ms. Namasivayam, Liliana Dhaliwal
+"""
+
+class FSM:
+    def __init__(self, initial_state):
+        # Dictionary (input_symbol, current_state) --> (action, next_state).
+        self.state_transitions = {}
+        self.current_state = initial_state
+
+    def add_transition(self, input_symbol, state, action=None, next_state=None):
+        if next_state is None:
+            self.state_transitions[(input_symbol, state)] = (action, self.current_state)
+        else:
+            self.state_transitions[(input_symbol, state)] = (action, next_state)
+
+    def get_transition(self, input_symbol, state):
+        return self.state_transitions[(input_symbol, state)]
+        
+    def process(self, input_symbol):
+        (action, next_state) = self.get_transition(input_symbol, self.current_state)
+        
+        if action is not None:
+            action()
+
+        self.current_state = next_state
